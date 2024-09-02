f9e58bdce2239e574e1970ccf22ea321498e1f9d
sachariase99 <sachariase99@hotmail.com>
2024-09-02 16:52:15 +0200

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        'max-height': 'max-height',
+      },
+    },
   },
   plugins: [],
 }
