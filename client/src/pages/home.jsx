a2657fb7de90e3827c3360a62efdcd613ea91664
sachariase99 <sachariase99@hotmail.com>
2024-09-02 16:52:39 +0200

    Update home.jsx

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,28 +1,13 @@
-import homeHeaderBg from "../assets/malerspande.jpg";
-import useArticles from "../hooks/useArticles";
+import HeaderCards from "../components/headerCards";
+import LoginPage from "./login";
 
 const Home = () => {
-  const { articles, loading, error } = useArticles(true);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+  
 
   return (
-    <div className="h-screen">
-      <div className="relative -top-32">
-        <img
-          className="w-full bg-cover"
-          src={homeHeaderBg}
-          alt="Header Background Image"
-        />
-        <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
-          {articles.slice(0, 2).map((article) => (
-            <div key={article.id}>
-              <p>{article.title}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+    <div className="">
+      <HeaderCards />
+
     </div>
   );
 };
