4962fd7ee78b9777fab6d9dd30192d9146a579e3
sachariase99 <sachariase99@hotmail.com>
2024-09-02 12:35:47 +0200

    Update appRoutes.jsx

--- client/src/router/appRoutes.jsx
+++ client/src/router/appRoutes.jsx
@@ -3,12 +3,14 @@ import Layout from "../layout/layout";
 import LoginPage from "../pages/login";
 import UserPage from "../pages/user";
 import RegisterPage from "../pages/register";
+import Home from "../pages/home";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/user" element={<UserPage />} />
