f42222d331ae48de8a2fd357692c96350cf1cfb7
sachariase99 <sachariase99@hotmail.com>
2024-09-03 00:34:15 +0200

    Update articleDetailPage.jsx
    Added loading

--- client/src/pages/articleDetailPage.jsx
+++ client/src/pages/articleDetailPage.jsx
@@ -6,7 +6,12 @@ const ArticleDetailPage = () => {
   const { id } = useParams();
   const { articles, loading, error } = useArticles(true);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl">
+        Loading...
+      </p>
+    );
   if (error) return <p>Error: {error}</p>;
 
   const article = articles.find((article) => article.id === parseInt(id));
@@ -16,7 +21,7 @@ const ArticleDetailPage = () => {
   // Convert the published_at string to a Date object
   const publishedDate = new Date(article.published_at);
 
-  // Format the date as "Day, Month Date, Year"
+  // Format the date as "Day, Month, Year"
   const formattedDate = publishedDate.toLocaleDateString('da-DK', {
     year: 'numeric',
     month: 'long',
@@ -24,13 +29,13 @@ const ArticleDetailPage = () => {
   });
 
   return (
-    <div className='mb-64 px-12 py-12' style={{ backgroundImage: "linear-gradient(to bottom, #D8EADB 40%, #FFFFFF 50%)" }}>
+    <article className='mb-64 px-12 py-12' style={{ backgroundImage: "linear-gradient(to bottom, #D8EADB 40%, #FFFFFF 50%)" }}>
       <h1 className='text-4xl font-bold'>{article.title}</h1>
       <p className='text-3xl font-bold text-[#119B1E] mt-6'>{article.teaser}</p>
       <p className='my-6'>{formattedDate}</p> {/* Display formatted date */}
       <img className='mb-6' src={article.image_url} alt={article.title} />
       <div className={styles.html_content} dangerouslySetInnerHTML={{ __html: article.html_content }} />
-    </div>
+    </article>
   );
 };
 
