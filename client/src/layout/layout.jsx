74e1599ceb62cfe58bd631171e9e7aad93df7c98
sachariase99 <sachariase99@hotmail.com>
2024-09-02 18:31:10 +0200

    Update layout.jsx

--- client/src/layout/layout.jsx
+++ client/src/layout/layout.jsx
@@ -12,16 +12,12 @@ const Layout = () => {
       <main
         style={{
           backgroundImage:
-            "linear-gradient(to bottom, #06682D 10%, #FFFFFF 70%)",
+            "linear-gradient(to bottom, #06682D 10%, #FFFFFF 45%)",
         }}
         className="w-full relative" // Ensure main takes full width
       >
         <div
           className="relative z-10 flex-grow max-w-[1280px] mx-auto px-8 xl:px-0"
-          style={{
-            backgroundImage:
-              "linear-gradient(to bottom, #D8EADB, rgba(255, 255, 255, 0))",
-          }}
         >
           <Outlet />
         </div>
