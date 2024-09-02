c10b3b24d968c86523fd35198058b847f9c2cd21
sachariase99 <sachariase99@hotmail.com>
2024-09-02 12:35:08 +0200

    Update nav.jsx
    Added Navbar

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,10 +1,62 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav>
+    <nav className="border-y-2 border-black">
+      <ul className="max-w-[1280px] mx-auto flex">
+        <NavLink
+          to="/home"
+          className={({ isActive }) =>
+            `py-8 px-12 text-xl border-l-2 border-r-2 border-black ${
+              isActive ? "bg-gray-300" : ""
+            }`
+          }
+        >
+          Forside
+        </NavLink>
+        <NavLink
+          to="/sortering"
+          className={({ isActive }) =>
+            `py-8 px-8 text-xl border-r-2 border-black ${
+              isActive ? "bg-gray-300" : ""
+            }`
+          }
+        >
+          Sorteringsguide
+        </NavLink>
+        <NavLink
+          to="/genbrug"
+          className={({ isActive }) =>
+            `py-8 px-8 text-xl border-r-2 border-black ${
+              isActive ? "bg-gray-300" : ""
+            }`
+          }
+        >
+          Genbrug
+        </NavLink>
+        <NavLink
+          to="/artikler"
+          className={({ isActive }) =>
+            `py-8 px-8 text-xl border-r-2 border-black ${
+              isActive ? "bg-gray-300" : ""
+            }`
+          }
+        >
+          Artikler
+        </NavLink>
+        <NavLink
+          to="/bestil"
+          className={({ isActive }) =>
+            `py-8 px-8 text-xl border-r-2 border-black ${
+              isActive ? "bg-gray-300" : ""
+            }`
+          }
+        >
+          Bestil Container
+        </NavLink>
+      </ul>
     </nav>
-  )
+  );
 };
 
 export default Nav;
