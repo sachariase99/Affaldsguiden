a2ef193feb61c78977e6b50678baa204ffbe49bb
sachariase99 <sachariase99@hotmail.com>
2024-09-04 00:11:38 +0200

    Update layout.jsx

--- client/src/layout/layout.jsx
+++ client/src/layout/layout.jsx
@@ -1,10 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import { useBackgroundColor } from "../context/BackgroundColorContext";
+import { useEffect } from "react";
 
 const Layout = () => {
   const { bgColor } = useBackgroundColor();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    // Navigate to /home if the current path is /
+    if (location.pathname === "/") {
+      navigate("/home");
+    }
+  }, [location, navigate]);
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -20,7 +30,10 @@ const Layout = () => {
       >
         <div
           className="z-10 flex-grow max-w-[1280px] mx-auto px-8 xl:px-0 relative"
-          style={{ backgroundImage: "linear-gradient(to bottom, #D8EADB 40%, #FFFFFF 50%)" }}
+          style={{
+            backgroundImage:
+              "linear-gradient(to bottom, #D8EADB 40%, #FFFFFF 50%)",
+          }}
         >
           <Outlet />
         </div>
