e6f02ec3b0ada8dc0f1fd1d1e588b178a7392a67
sachariase99 <sachariase99@hotmail.com>
2024-09-04 19:06:49 +0200

    Update layout.jsx

--- client/src/layout/layout.jsx
+++ client/src/layout/layout.jsx
@@ -9,12 +9,45 @@ const Layout = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const pageTitles = {
+    "/": "Home Page",
+    "/home": "Home Page",
+    "/sortingGuide": "Sorteringsguide",
+    "/recycle": "Genbrugsstationer",
+    "/articles": "Artikler",
+    "/container": "Bestil Container",
+  };
+
+  // Function to get title for the given path
+  const getTitle = (path) => {
+    // Return the title for static routes
+    if (pageTitles[path]) return pageTitles[path];
+
+    // Check for dynamic routes by matching base paths
+    const basePaths = ["/sortingGuide", "/recycle", "/articles"];
+    for (const basePath of basePaths) {
+      if (path.startsWith(basePath)) {
+        return pageTitles[basePath] || "Default Title";
+      }
+    }
+
+    // Default title if no match
+    return "Default Title";
+  };
+
   useEffect(() => {
-    // Navigate to /home if the current path is /
+    // Redirect to /home if the current path is /
     if (location.pathname === "/") {
       navigate("/home");
     }
   }, [location, navigate]);
+
+  useEffect(() => {
+    // Set the document title based on the current path
+    const path = location.pathname;
+    const title = getTitle(path);
+    document.title = title; // Set the document title
+  }, [location.pathname]);
 
   return (
     <div className="flex flex-col min-h-screen">
