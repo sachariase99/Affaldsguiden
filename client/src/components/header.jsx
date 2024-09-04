ca2a252344686dc8d8fa9d1ba01317956784c203
sachariase99 <sachariase99@hotmail.com>
2024-09-04 04:04:31 +0200

    Update header.jsx

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,38 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { IoMdLogIn } from "react-icons/io";
 import logo from "../assets/logo.png";
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
-import Nav from './nav'
+import Nav from './nav';
 
 const Header = () => {
   const { isLoggedIn, logout } = useContext(AuthContext);
+  const navigate = useNavigate(); // Hook to handle navigation
+
+  const handleLogout = async () => {
+    await logout(); // Ensure logout completes
+    navigate('/home'); // Redirect to /home after logout
+  };
 
   return (
     <div>
       <div className="flex items-center justify-between gap-8 mx-24 py-4 bg-white z-50">
-      <Link to="/home">
-        <img src={logo} alt="Logo" />
-      </Link>
-      {isLoggedIn ? (
-        <button
-          className="bg-[#D8EADB] hover:bg-[#D6BD98] rounded-xl px-8 py-2 flex items-center gap-2 text-xl"
-          onClick={logout}
-        >
-          Logout
-          <IoMdLogIn className="rotate-180"/>
-        </button>
-      ) : (
-        <Link
-          className="bg-[#D8EADB] hover:bg-[#D6BD98] rounded-xl px-8 py-2 flex items-center gap-2 text-xl"
-          to="/login"
-        >
-          Login
-          <IoMdLogIn />
+        <Link to="/home">
+          <img src={logo} alt="Logo" />
         </Link>
-      )}
-    </div>
-    <Nav />
+        {isLoggedIn ? (
+          <button
+            className="bg-[#D8EADB] hover:bg-[#D6BD98] rounded-xl px-8 py-2 flex items-center gap-2 text-xl"
+            onClick={handleLogout} // Use handleLogout function
+          >
+            Logout
+            <IoMdLogIn className="rotate-180"/>
+          </button>
+        ) : (
+          <Link
+            className="bg-[#D8EADB] hover:bg-[#D6BD98] rounded-xl px-8 py-2 flex items-center gap-2 text-xl"
+            to="/login"
+          >
+            Login
+            <IoMdLogIn />
+          </Link>
+        )}
+      </div>
+      <Nav />
     </div>
   );
 };
